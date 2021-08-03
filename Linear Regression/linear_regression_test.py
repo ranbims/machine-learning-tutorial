8585d1cc7c15c41e3aca8d4aadb536f71b478106
Ran <msnbiran@live.cn>
2021-08-03 23:53:56 +0800

    [Linear Regression] Add training data and load logic

---
+++ Linear Regression/linear_regression_test.py
@@ -0,0 +1,31 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from typing import List, Tuple
+
+def loadTrainingData() -> Tuple[List[float], List[float]]:
+    X = []
+    Y = []
+    with open("lianjia_2021_8_3.txt") as file:
+        lines = file.readlines()
+        for line in lines:
+            value = line.strip().split("\t")
+            X.append(float(value[0]))
+            Y.append(float(value[1]))
+    return (X, Y)
+
+def toNumpyArray(X: List[float], Y: List[float]) -> Tuple[np.ndarray, np.ndarray]:
+    return (np.array(X).reshape(len(X), 1), np.array(Y).reshape(len(Y), 1))
+
+def plotData(X: List[float], Y: List[float]):
+    XS, YS = zip(*sorted(zip(X, Y)))
+    plt.plot(XS, YS, 'ro')
+    plt.show()
+
+def main():
+    X_raw, Y_raw = loadTrainingData()
+    X, Y = toNumpyArray(X_raw, Y_raw)
+    plotData(X, Y)
+    pass
+
+if __name__ == "__main__":
+    main()
